d4effae6f2524f6138fd7d2e7cb6521c4c2e1763
benkosz24d <benkosz.24d@acsjszki.hu>
2025-02-25 08:44:46 +0100

    1.update

---
+++ script.js
@@ -0,0 +1,44 @@
+console.log("Szia");
+
+
+
+console.log(500);
+
+
+console.log(true);
+console.log(false);
+
+//logikai ÉS
+// (boolean,boolean) ->boolean
+ 
+//negálás
+//....
+
+//undifined
+console.log(undefined); 
+
+//null
+ 
+console.log(null);
+
+
+
+/*
+* Array (tömb)
+*/
+
+//Array<_>
+//Array of ?
+
+//Array of(string)
+
+console.log(["elso","masodik", "harmadik"])
+//Array of numbers
+console.log([2, 12, 3])
+
+//szűrés
+//sorba rendezés
+//hozzá adhatunk új elemet
+//aggregálás
+
+console.log([2, 12, 3][1])
